4679f86fa947945003315497a8a2bed0061449ce
Ivo Oliveira <soidesigncriacao@gmail.com>
2018-12-08 01:02:41 -0200

    Iniciando Projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
